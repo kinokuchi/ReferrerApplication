59d0eb909c54bc3fce97abf0c5be357a576e1241
kinokuchi <inokuchi.keisuke@armon.co.jp>
2019-08-20 10:46:35 +0900

    初回コミット

---
+++ app/src/main/java/com/example/referrerapplication/ReferrerReceiver.java
@@ -0,0 +1,21 @@
+package com.example.referrerapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ReferrerReceiver extends BroadcastReceiver {
+    private static final String KEY_REFERRER = "referrer";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String referrer = intent.getStringExtra(KEY_REFERRER);
+        sendConversion(referrer);
+    }
+
+    private void sendConversion(String referrer) {
+        // send this referrer string to your server
+        Log.v("Referrer", "referrer=" + referrer);
+    }
+}
